9cfb04e51aaae053e3200e651fa9ca564e875860
muriloschiler <muriloschiler.ls@gmail.com>
2021-11-04 16:07:41 -0300

    Initial commit

---
+++ CSV_Scraper_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSV_Scraper_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
